53ee05ceb41e4f28f26a78a6cf2221628826db8d
DanielPetelin <daniilpetelin77@gmail.com>
2020-04-25 16:09:38 +0300

    add all

--- js/script.js
+++ js/script.js
@@ -1,17 +1,13 @@
 document.onclick = () => {
-    var delPatrons = +document.querySelector('.patrons').value--;
+    let delPatrons = +document.querySelector('.patrons').value--;
 
     if (delPatrons == 0) {
         +document.querySelector('.patrons').value++;
-        checkKey;
     };
 };
 
-function checkKey(e) {
-    e = e || window.event;
-
-    if (delPatrons == 0 && e.keyCode == 82) {
-        +document.querySelector('.patrons').value + 5;
-        +document.querySelector('.all').value - 5;  
-    };
-};
+document.addEventListener('keydown', function(event) {
+    if (event.code == 'KeyR') {
+        document.querySelector('.patrons').value = 5;
+    }
+});
