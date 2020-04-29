428da5011deb5460cdea56d9f86054e896108150
DanielPetelin <daniilpetelin77@gmail.com>
2020-04-29 19:22:03 +0300

    add new sound and refact code

--- js/script.js
+++ js/script.js
@@ -1,23 +1,30 @@
 document.onclick = () => {
-    let delPatrons = +document.querySelector('.patrons').value--;
+    let delPatron = +document.querySelector('.patrons').value--;
     let audio = new Audio();
-	audio.src = './sounds/Выстрел.mp3';
-	audio.autoplay = true;
+    audio.src = './sounds/Выстрел.mp3';
+    let audioEmpty = new Audio();
+    audioEmpty.src = './sounds/пустой выстрел.wav';
 
-    if (delPatrons == 0) {
-        +document.querySelector('.patrons').value++;
-        audio.autoplay = false;
+    if (document.querySelector('.patrons').value <= 5 && document.querySelector('.patrons').value >= 0) {
+        audio.autoplay = true;   
+        audioEmpty.autoplay = false;
+    } else {
+        +document.querySelector('.patrons').value++; 
+
+        audioEmpty.autoplay = true;
+        audio.autoplay = false;  
     };
 };
 
 document.addEventListener('keydown', function(event) {
+    let audio3 = new Audio();
+	audio3.src = './sounds/перезарядка.wav';
+
     if (event.code == 'KeyR' && document.querySelector('.patrons').value < 5) {
-        let audio2 = new Audio();
-	    audio2.src = './sounds/перезарядка.wav';
-        audio2.autoplay = true;
+        audio3.autoplay = true;
         
         document.querySelector('.patrons').value = 5; 
     } else {
-        audio2.outoplay = false;
+        audio3.outoplay = false;
     };
 });
